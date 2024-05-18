9907b83eda77e083a567686d3cb35881dde0c97c
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-05-18 18:37:54 -0300

    rfc: making the Application class better

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,16 +3,10 @@
 use Yui\Application;
 
 /**
- * Configure the application to be bootstrapped.
+ * Create a new Yui application instance.
  */
 return Application::configure()
 	->with(
 		basePath: dirname(__DIR__),
-		bootstrapPath: dirname(__DIR__).'/bootstrap',
-		databasePath: dirname(__DIR__).'/app/Database',
-		publicPath: dirname(__DIR__).'/public',
-		resourcePath: dirname(__DIR__).'/resources',
-		storagePath: dirname(__DIR__).'/storage',
-		routesPath: dirname(__DIR__).'/app/Routes',
 	)
 	->create();
