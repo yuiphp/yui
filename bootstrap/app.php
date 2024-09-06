2fa3c7b84b746487c7483a622d920aa42d37519d
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2024-09-05 21:56:21 -0300

    wip

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,8 +5,7 @@ use Yui\Application;
 /**
  * Create a new Yui application instance.
  */
-return Application::configure()
-	->with(
-		basePath: dirname(__DIR__),
-	)
-	->create();
+return Application::configure(
+    [
+        'basePath' => dirname(__DIR__),
+    ])->build();
